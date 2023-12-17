d398dadd2a3daa960bf31c26db3aa6a14201a9d1
Kangdyu <kangdyu99@gmail.com>
2023-12-17 22:09:15 +0900

    chore(06): comment about response_model & return type

--- 06-basic-calc/main.py
+++ 06-basic-calc/main.py
@@ -8,6 +8,13 @@ class CalcResponse(BaseModel):
     result: int
 
 
+"""
+NOTE: response_model과 explicit return type 모두 같은 결과를 반환함
+editor/type tool support를 위해 return type을 사용하는 쪽이 권장되는 것으로 보임
+SEE: https://fastapi.tiangolo.com/tutorial/response-model/#return-type-and-data-filtering
+"""
+
+
 @app.get("/sum")
 def get_sum(a: int, b: int) -> CalcResponse:
     return CalcResponse(result=a + b)
@@ -15,4 +22,4 @@ def get_sum(a: int, b: int) -> CalcResponse:
 
 @app.get("/mul", response_model=CalcResponse)
 def get_mul(a: int, b: int):
-    return {"result": a * b}
+    return CalcResponse(result=a * b)
